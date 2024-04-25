55adc4df00a8fffa35183949c9e72b37fcef05e0
valentynovych <sasha.lesik.98@gmail.com>
2024-04-25 22:39:19 +0300

    initial commit

---
+++ src/main/java/com/example/housingsystem/HousingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.housingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HousingSystemApplication.class, args);
+    }
+
+}
